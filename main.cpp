994834101d74badf66e0f51a47ae8ae60918d021
Chris King <G.nius.ck@gmail.com>
2015-01-21 12:12:27 -0500

    Update Shell

--- main.cpp
+++ main.cpp
@@ -1,45 +1,36 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
 unsigned int medicine = 0;
 unsigned int pet_feed = 0;
 unsigned int toy = 0;
 unsigned int books = 0;
-
 void loop(string name, string species);
 unsigned int store(unsigned int money);
 string name();
 string species();
 void end();
-
 int main()
 {
-    
+    return;
 }
-
 void loop(string name, string species)
 {
-
+    return;
 }
-
 unsigned int store(unsigned int money)
 {
-    
+    return 10;
 }
-
 string name()
 {
-
+    return "Name";
 }
-
 string species()
 {
-
+    return "Species";
 }
-
-string end()
+void end()
 {
-
+    cout << "End" << endl;
 }
