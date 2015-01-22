9b2323fa733704f0f8cb7410d15f41709f7d9d0a
Chris King <G.nius.ck@gmail.com>
2015-01-22 13:34:47 -0500

    Added menu and inventory couts

--- main.cpp
+++ main.cpp
@@ -12,11 +12,41 @@ string species();
 void end();
 int main()
 {
-    return;
+    loop(name(), species());
 }
 void loop(string name, string species)
 {
-    return;
+    float health = 10;
+    float nutrition = 10;
+    float happiness = 10;
+    float intelligence = 10;
+    unsigned int money = 0;
+
+    while(true)
+    {
+        cout << "Here is your inventory: "
+             << "Medicine: " << medicine << endl
+             << "Food: " << pet_feed << endl
+             << "Toys: " << toy << endl
+             << "Books: " << books << endl;
+
+        cout << "A) Medicate your pet" << endl
+             << "B) Feed your pet" << endl
+             << "C) Entertain your pet" << endl
+             << "D) Educate your pet" << endl;
+
+        char answer;
+        cin << answer;
+        switch(answer)
+        {
+        case 'A':
+            if (medicine == 0)
+            {
+                cout << "Sorry, you have no medicine." << endl;
+            }
+        }
+    }
+
 }
 unsigned int store(unsigned int money)
 {
