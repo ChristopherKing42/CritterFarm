e600e8f859df0528e32a504337d8638c6a4049a2
Aquaticsnipes <aquaticsnipes@gmail.com>
2015-01-27 13:33:58 -0500

    store update 2

--- main.cpp
+++ main.cpp
@@ -2,22 +2,46 @@
 //#include <studio>
 
 
-unsigned int store(unsigned int money)
+unsigned int store(unsigned int money
 {
+
+    unsigned int amount_of_item;
     unsigned int bank_account;
     string action;
     string buy("buy");
     string sell("sell");
     
     cout<<"Howd'y there!  What can I do for you today?"<<endl;
-    cout<<"Would you like to buy or sell?"<<endl;
+    cout<<endl;
+    cout<<"Would you like to buy or sell ?"<<endl;
     cout<<endl;
     getline(cin, action);
     
     isupper(action) = tolower(action);
     
-    if(action==sell)
+    if(action=="sell")
     {
+        string loop;
+        float profit;
+        float pancake=0.75;
+        float apples=0.25;
+        float medicine=2.00;
+        float 
+        string item;
+        cout<<"What item would you like to sell? ";
+        getline(cin, item)
+        isupper(item) = tolower(item);
+        for(loop!="exit")
+        switch(string(item))
+        {
+            case "pancakes":
+            
+            cout<<"How many pancakes would you like to sell? ";
+            cin>>amount_of_item;
+            profit=profit+pancake;
+            
+        }
+        
         
     }
     
