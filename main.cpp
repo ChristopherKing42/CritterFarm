3de659ea7c0cb101b32642c75ded5b4b876cbb08
Aquaticsnipes <aquaticsnipes@gmail.com>
2015-01-28 13:05:51 -0500

    stroe u pdate 3

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 //#include <ctype>
 //#include <studio>
+//#include <windows.h>
 
 
-unsigned int store(unsigned int money
+unsigned int store(unsigned int money)
 {
 
     unsigned int amount_of_item;
     unsigned int bank_account;
-    string action;
+    string action; 
     string buy("buy");
     string sell("sell");
     
@@ -22,23 +23,45 @@ unsigned int store(unsigned int money
     if(action=="sell")
     {
         string loop;
+        float cost;
         float profit;
         float pancake=0.75;
         float apples=0.25;
         float medicine=2.00;
-        float 
+         
+        for(loop!="exit")
+        {
         string item;
         cout<<"What item would you like to sell? ";
         getline(cin, item)
         isupper(item) = tolower(item);
-        for(loop!="exit")
-        switch(string(item))
-        {
-            case "pancakes":
-            
+            if(item=="pancakes")
+            {
             cout<<"How many pancakes would you like to sell? ";
             cin>>amount_of_item;
-            profit=profit+pancake;
+            cost=pancake*amount_of_item;
+            profit=profit+cost;
+            }
+            else
+            cout<<"Sorry that is not an appropriate response. Try again."<<endl;
+            
+            if(item=="apples")
+            {
+            cout<<"How many apples would you like to sell? ";
+            cin>>amount_of_item;
+            cost=apples*amount_of_item;
+            profit=profit+cost;
+            }
+            else
+            cout<<"Sorry that is not an appropriate response. Try again."<<endl;
+            
+            if(item=="medicine")
+            {
+            cout<<"How many vials of your medication would you like to sell? "<<endl;
+            cin>>amount_of_item;
+            cost=medicine*amount_of_item;
+            profit=profit+cost;
+            }
             
         }
         
