a9f094b613a57035d17e5ce134f306ec48878a9f
Aquaticsnipes <aquaticsnipes@gmail.com>
2015-01-30 12:49:40 -0500

    update 4

--- main.cpp
+++ main.cpp
@@ -1,40 +1,37 @@
-//#include <ctype>
-//#include <studio>
-//#include <windows.h>
-
-
-unsigned int store(unsigned int money)
+float store(float money)
 {
+    string item;
     string loop;
     unsigned int amount_of_item;
     unsigned int bank_account;
-    string action; 
+    string action;
     string buy("buy");
     string sell("sell");
-    
+
     cout<<"Howd'y there!"<<endl;
-    cout<<"You have now entered the store."
+    cout<<"You have now entered the store."<<endl;
     cout<<endl;
-    cout<<"Commands include \"buy\", \"sell\", or \"exit\"."
+    cout<<"Commands include \"buy\", \"sell\", or \"exit\"."<<endl;
     cout<<endl;
     cout<<"Would you like to buy or sell ?"<<endl;
     cout<<endl;
     getline(cin, action);
     getline(cin, item);
-    
-    isupper(action) = tolower(action);
-    isupper(item) = tolower(item);
-    
+
+
+    transform(action.begin(), action.end(), action.begin(), ::tolower);
+    transform(item.begin(), item.end(), item.begin(), ::tolower);
+
     if(action=="sell")
     {
-        
+
         float cost;
         float profit;
         float pancake=0.75;
         float apples=0.25;
         float medicine=2.00;
-         
-        for(loop!="exit")
+
+        do
         {
         string item;
         cout<<"Please select an item from the list below"<<endl;
@@ -45,8 +42,10 @@ unsigned int store(unsigned int money)
         cout<<"     * Pancakes"<<endl;
         cout<<endl;
         cout<<"What item would you like to sell? ";
-        getline(cin, item)
-        isupper(item) = tolower(item);
+        getline(cin, item);
+
+         transform(item.begin(), item.end(), item.begin(), ::tolower);
+
             if(item=="pancakes")
             {
             cout<<"How many pancakes would you like to sell? ";
@@ -56,7 +55,7 @@ unsigned int store(unsigned int money)
             }
             else
             cout<<"Sorry that is not an appropriate response. Try again."<<endl;
-            
+
             if(item=="apples")
             {
             cout<<"How many apples would you like to sell? ";
@@ -66,7 +65,7 @@ unsigned int store(unsigned int money)
             }
             else
             cout<<"Sorry that is not an appropriate response. Try again."<<endl;
-            
+
             if(item=="medicine")
             {
             cout<<"How many vials of your medication would you like to sell? "<<endl;
@@ -74,58 +73,65 @@ unsigned int store(unsigned int money)
             cost=medicine*amount_of_item;
             profit=profit+cost;
             }
-            
+
         }
-        
-        
+        while(loop!="exit");
     }
-    if(action=="sell")
+    if(action=="buy")
     {
         string loop;
         float cost;
         float profit;
-        float pancake=0.75;
-        float apples=0.25;
-        float medicine=2.00;
-         
-        for(loop!="exit")
+        float pancake=1.25;
+        float apples=.50;
+        float medicine=3.50;
+
+        do
         {
         string item;
+        cout<<"Please select an item from the list below"<<endl;
+        cout<<endl;
+        cout<<"     * Apples"<<endl;
+        cout<<"     * Books"<<endl;
+        cout<<"     * Medicine"<<endl;
+        cout<<"     * Pancakes"<<endl;
+        cout<<endl;
         cout<<"What item would you like to buy? ";
-        getline(cin, item)
-        isupper(item) = tolower(item);
+        getline(cin, item);
+
+        transform(item.begin(), item.end(), item.begin(), ::tolower);
+
             if(item=="pancakes")
             {
             cout<<"How many pancakes would you like to buy? ";
             cin>>amount_of_item;
             cost=pancake*amount_of_item;
-            money=money+cost;
+            money=money-cost;
             }
             else
             cout<<"Sorry that is not an appropriate response. Try again."<<endl;
-            
+
             if(item=="apples")
             {
             cout<<"How many apples would you like to buy? ";
             cin>>amount_of_item;
             cost=apples*amount_of_item;
-            money=money+cost;
+            money=money-cost;
             }
             else
             cout<<"Sorry that is not an appropriate response. Try again."<<endl;
-            
+
             if(item=="medicine")
             {
             cout<<"How many vials of your medication would you like to buy? "<<endl;
             cin>>amount_of_item;
             cost=medicine*amount_of_item;
-            money=money+cost;
+            money=money-cost;
             }
-            
+
         }
-        
-        
+        while(loop!="exit");
     }
-    
-    return bank_account;
+
+    return money;
 }
